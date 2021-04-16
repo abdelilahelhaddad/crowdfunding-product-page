f2d6c81967f36e2e4c52cde9a60fb39b5ce0c789
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-04-16 15:34:28 +0000

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- script.js
+++ script.js
@@ -84,11 +84,16 @@ bookmark.addEventListener("click", () => {
 const open_nav = document.querySelector('#open_nav');
 const close_nav = document.querySelector('#close_nav');
 const nav_modalo = document.getElementById('ham-nav-modal_container');
+const logo = document.querySelector(".logo");
 
 open_nav.addEventListener('click', () => {
   nav_modalo.classList.add('show');
+  open_nav.style.visibility = "hidden";
+  logo.style.visibility = "hidden";
 });
 
 close_nav.addEventListener('click', () => {
   nav_modalo.classList.remove('show');
+  open_nav.style.visibility = "visible";
+  logo.style.visibility = "visible";
 });
